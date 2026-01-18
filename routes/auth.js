37644f203a4763b0cf247d3124b6eef37206dc30
rakesh <rrak37458@gmail.com>
2026-01-18 16:33:06 +0530

    Updated code

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { login } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth.js";
+import multer from "multer";
 
 const router = express.Router();
 
+// Multer setup for handling profile picture uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+// Routes
+router.post("/register", upload.single("picture"), register);
 router.post("/login", login);
 
 export default router;
